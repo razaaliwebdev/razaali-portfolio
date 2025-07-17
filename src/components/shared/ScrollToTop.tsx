9ce2bc2797581ea01b121ca0af5e45d193b2e110
razaaliwebdev <razaali.webdev@gmail.com>
2025-07-17 10:54:28 +0500

    fix the scroll top button

--- src/components/shared/ScrollToTop.tsx
+++ src/components/shared/ScrollToTop.tsx
@@ -6,10 +6,10 @@ import { ChevronUp } from "lucide-react";
 const ScrollToTop = () => {
   return (
     <Button
-      className="fixed bottom-6 right-6 z-50 bg-brand-primary text-white  shadow-lg hover:trannsform-y-1 text-4xl     cursor-pointer transition"
+      className="fixed bottom-6  hover:bg-brand-primary/80 right-6 z-50 bg-brand-primary  shadow-lg  text-4xl  cursor-pointer transition"
       onClick={() => window.scroll({ top: 0, behavior: "smooth" })}
     >
-      <ChevronUp className="w-6 h-6 text-3xl" />
+      <ChevronUp className="w-6 h-6 text-3xl font-bold text-black" />
     </Button>
   );
 };
